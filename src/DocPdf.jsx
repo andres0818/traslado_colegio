875b00e6a0537b257e39797d237cd4fbd85a7b80
andres0818 <c.andres.perez.h@gmail.com>
2023-03-21 12:03:01 -0500

    change styles

--- src/DocPdf.jsx
+++ src/DocPdf.jsx
@@ -8,46 +8,53 @@ import {
 } from "@react-pdf/renderer";
 import React from "react";
 
+const styles = {
+  container: {
+    width: "100%",
+    height: "100vh",
+    fontFamily: "helvetica",
+  },
+  page: {},
+  docPDF: {
+    padding: "50px",
+    textAlign: "justify",
+  },
+  infoText: {
+    gap: "35px",
+  },
+
+  img: {
+    width: "200px",
+  },
+};
+
 const DocPdf = ({ doc }) => {
   return (
-    <PDFViewer style={{ width: "100%", height: "100vh", }}>
+    <PDFViewer style={styles.container}>
       <Document>
         <Page size="A4">
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              fontStyle: "italic",
-              padding: "50px",
-            }}
-          >
-            <View>
-              <View style={{ gap: "30px" }}>
-                <Text>{`Medellín,${doc.date} `}</Text>
-                <Text>Señores:</Text>
-                <Text>FUNDACION BIBE</Text>
-                <Text>Ref. Cruce de saldo</Text>
-                <Text>
-                  Yo {` ${doc?.adultName && doc.adultName} `} identificado(a)
-                  con cédula N°
-                  {` ${doc?.adultId && doc.adultId} `} autorizo a la FUNDACIÓN
-                  BIBE identificada con Nit. 901.651.472-4 que el saldo a favor
-                  que tengo por concepto de Guías, del estudiante
-                  {` ${doc?.student && doc.student} `} del grado
-                  {` ${doc?.grade && doc.grade} `}, sea trasladado al COLEGIO
-                  MILITAR COLMILGPNO identificado con Nit. 811.015.863 para
-                  abono a la pensión.
-                </Text>
-              </View>
-              <View>
-                <Text>Cordialmente,</Text>
-                {doc?.firm && (
-                  <Image src={doc?.firm} style={{ width: "200px" }} />
-                )}
-                <Text>cc: {` ${doc?.adultId && doc.adultId} `}</Text>
-              </View>
+          <View style={styles.docPDF}>
+            <View style={styles.infoText}>
+              <Text>{`Medellín,${doc.date} `}</Text>
+              <Text>Señores:</Text>
+              <Text>FUNDACION BIBE</Text>
+              <Text>Ref. Cruce de saldo</Text>
+              <Text>
+                Yo {` ${doc?.adultName && doc.adultName} `} identificado(a) con
+                cédula N°
+                {` ${doc?.adultId && doc.adultId} `} autorizo a la FUNDACIÓN
+                BIBE identificada con Nit. 901.651.472-4 que el saldo a favor
+                que tengo por concepto de Guías, del estudiante
+                {` ${doc?.student && doc.student} `} del grado
+                {` ${doc?.grade && doc.grade} `}, sea trasladado al COLEGIO
+                MILITAR COLMILGPNO identificado con Nit. 811.015.863 para abono
+                a la pensión.
+              </Text>
+              <Text style={{ marginTop: "22px" }}>Cordialmente,</Text>
+            </View>
+            <View style={{}}>
+              {doc?.firm && <Image src={doc?.firm} style={styles.img} />}
+              <Text>cc: {` ${doc?.adultId && doc.adultId} `}</Text>
             </View>
           </View>
         </Page>
