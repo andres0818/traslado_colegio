161c5c5cb337d656193ab1f9b71adc716a35634b
andres0818 <c.andres.perez.h@gmail.com>
2023-03-22 22:20:17 -0500

    change title  document

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Link, Outlet,useLocation } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  
-  const location = useLocation()
+  const location = useLocation();
+  const [statePage, setStatePage] = useState(false);
 
+  useEffect(() => {
+    function handleVisibilityChange() {
+      setStatePage(document.hidden);
+    }
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    document.title = statePage
+      ? "Fundacion BIBE"
+      : location.pathname === "/"
+      ? "Formulario"
+      : "Listado";
+  }, [statePage,location.pathname]);
   return (
     <>
       <nav className="nav nav-tabs">
